7f1aefa5db05d63f1fd8311007659f7a87a59470
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-30 01:52:21 +0600

    navbar-designing

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,7 +1,7 @@
-
+import {GoTriangleDown} from "react-icons/go"
 export default function Navbar() {
     return (
-        <div className="UniversalPadding flex bg-white shadow-md items-center justify-between py-6 text-Primary">
+        <div className="NavbarPadding flex bg-white shadow-md items-center justify-between py-6 text-Primary">
             <div>
                 <img src="../../assets/Photos/logo-256x256.png" alt="" srcset="" />
             </div>
@@ -10,10 +10,10 @@ export default function Navbar() {
                 <li>Features</li>
                 <li>FAQ</li>
                 <li className="relative">
-                    <p>More</p>
+                    <p className="flex space-x-1 items-center">More <GoTriangleDown className="text-black"/></p>
                     <div className="absolute top-8 border rounded py-2 bg-white -left-1 text-black">
-                        <li className="dropdown transition duration-500 hover:bg-gray-100 py-1 pl-5 pr-20">Contact</li>
-                        <li className="dropdown transition duration-500 hover:bg-gray-100 py-1 pl-5 pr-20">Help</li>
+                        <li className="dropdown transition duration-300 hover:bg-gray-100">Contact</li>
+                        <li className="dropdown transition duration-300 hover:bg-gray-100">Help</li>
                     </div>
                 </li>
                 <li>Blog</li>
