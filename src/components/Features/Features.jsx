dd0e2808fa28e5b6756f2bb6e4bc9797e1598c76
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-30 05:38:12 +0600

    featured-part-start-working-on

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -2,10 +2,10 @@
 function Features() {
     return (
         <div className="bg-GrayBg">
-            <div className="text-center mb-5">
-                <h1 className="text-[50px] font-extrabold lora mb-10">Features</h1>
-                <p className="text-[22px] text-Primary">
-                Minimalist design, privacy-focused with high performance is been the key mantra for glimpses journal app in its journey in the last 4 years.
+            <div className="text-center py-8 FeatureIntro">
+                <h1 className="text-[32px] font-bold lora mb-10">Features</h1>
+                <p className="text-Primary text-center">
+                    Minimalist design, privacy-focused with high performance is been the key mantra for glimpses journal app in its journey in the last 4 years.
                 </p>
             </div>
             <div></div>
