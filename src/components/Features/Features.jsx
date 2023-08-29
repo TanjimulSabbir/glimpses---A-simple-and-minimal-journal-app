e071b42b6de11c87bab2ae1894863adec759bc3e
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-30 05:53:52 +0600

    featured-part-start-working-on-service.json.create.

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,14 +1,18 @@
+import {FaLeaf,FaLock,FaAnchor,FaCalendarAlt,FaDropbox,FaPaperPlane} from "react-icons/fa";
+
 
 function Features() {
     return (
-        <div className="bg-GrayBg">
+        <div className="bg-GrayBg raleway">
             <div className="text-center py-8 FeatureIntro">
                 <h1 className="text-[32px] font-bold lora mb-10">Features</h1>
                 <p className="text-Primary text-center">
                     Minimalist design, privacy-focused with high performance is been the key mantra for glimpses journal app in its journey in the last 4 years.
                 </p>
             </div>
-            <div></div>
+            <div className="text-[rgb(255, 203, 0)]">
+
+            </div>
         </div>
     )
 }
