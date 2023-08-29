2b9cb72e302e3052606c38c68c87b88b370b1c0b
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-30 02:17:11 +0600

    navbar-designing

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -5,20 +5,22 @@ import NavLogo from "../../assets/Photos/logo-256x256.png"
 export default function Navbar() {
     const [drowDown, setDropDown] = useState(false)
     return (
-        <div className="NavbarPadding flex bg-white shadow-md items-center justify-between py-6 text-Primary">
-            <div className="flex items-center space-x-1">
-                <img className="w-11 h-11" src={NavLogo} alt="glimpses" />
-                <span className="text-black font-bold">glimpses</span>
+        <div className="NavbarPadding flex bg-white shadow-md items-center justify-between py-4 text-Primary">
+            <div className="flex items-center pl-6">
+                <div className="relative">
+                    <img className="w-11 h-11" src={NavLogo} alt="glimpses" />
+                </div>
+                <span className="text-black text-xl font-bold leading-relaxed tracking-wide ">glimpses</span>
             </div>
-            <div className="flex space-x-5 items-center">
+            <div className="flex space-x-5 items-center pr-14">
                 <li>Home</li>
                 <li>Features</li>
                 <li>FAQ</li>
-                 {/* More Button */}
+                {/* More Button */}
                 <li className="relative" onClick={() => setDropDown(!drowDown)}>
-                   
+
                     <p className={`${drowDown && "text-black"} flex space-x-1 items-center`}>
-                        More 
+                        More
                         <GoTriangleDown className="text-black" />
                     </p>
                     {/* DropwDown Menut */}
