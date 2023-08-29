bc76e160810fba4701369e50072e1be801f70207
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-30 01:55:01 +0600

    navbar-designing

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,5 +1,7 @@
-import {GoTriangleDown} from "react-icons/go"
+import { useState } from "react"
+import { GoTriangleDown } from "react-icons/go"
 export default function Navbar() {
+    const [drowDown, setDropDown] = useState(false)
     return (
         <div className="NavbarPadding flex bg-white shadow-md items-center justify-between py-6 text-Primary">
             <div>
@@ -9,9 +11,15 @@ export default function Navbar() {
                 <li>Home</li>
                 <li>Features</li>
                 <li>FAQ</li>
-                <li className="relative">
-                    <p className="flex space-x-1 items-center">More <GoTriangleDown className="text-black"/></p>
-                    <div className="absolute top-8 border rounded py-2 bg-white -left-1 text-black">
+                 {/* More Button */}
+                <li className="relative" onClick={() => setDropDown(!drowDown)}>
+                   
+                    <p className={`${drowDown && "text-black"} flex space-x-1 items-center`}>
+                        More 
+                        <GoTriangleDown className="text-black" />
+                    </p>
+                    {/* DropwDown Menut */}
+                    <div className={`${drowDown ? "block" : "hidden"} absolute top-8 border rounded py-2 bg-white -left-1 text-black`}>
                         <li className="dropdown transition duration-300 hover:bg-gray-100">Contact</li>
                         <li className="dropdown transition duration-300 hover:bg-gray-100">Help</li>
                     </div>
