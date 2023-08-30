f0a15f990b980b433579a3662f13a4aa00424feb
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-30 06:51:25 +0600

    featured-part-start-working-on-service.json.create.

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,11 +1,8 @@
-import { FaLeaf, FaLock, FaAnchor, FaCalendarAlt, FaDropbox, FaPaperPlane } from "react-icons/fa";
 import Services from "./Services";
 
-
 function Features() {
-    const Icons = { FaLeaf, FaLock, FaAnchor, FaCalendarAlt, FaDropbox, FaPaperPlane };
     return (
-        <div className="bg-GrayBg raleway">
+        <div className="bg-GrayBg raleway pb-16">
             <div className="text-center py-8 FeatureIntro">
                 <h1 className="text-[32px] font-bold lora mb-10">Features</h1>
                 <p className="text-Primary text-center">
--- src/components/Features/Services.jsx
+++ src/components/Features/Services.jsx
@@ -13,14 +13,14 @@ const iconComponents = {
 
 function Services() {
     return (
-        <div className='Container grid grid-cols-2 lg:grid-cols-3 max-w-6xl mx-auto gap-10'>
+        <div className='Container grid grid-cols-2 lg:grid-cols-3 max-w-6xl mx-auto gap-8'>
             {ServicesData.Services.map((service, index) => {
                 const { icon, name, description } = service;
                 const IconComponent = iconComponents[icon]; // Get the corresponding icon component
                 return (
-                    <div key={index} className='flex space-x-4 justify-center'>
+                    <div key={index} className='flex space-x-6 justify-center'>
                         <div>
-                            <IconComponent className="text-[#FFCB00] text-5xl" />
+                            <IconComponent className="text-[#FFCB00] text-[40px]" />
                         </div>
                         <div className='-mt-2'>
                             <h3 className='text-xl text-Secondary font-bold mb-3'>{name}</h3>
