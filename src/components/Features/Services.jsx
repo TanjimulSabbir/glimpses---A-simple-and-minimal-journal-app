694221b02dc34515ca7b6fed6112e19b89d3c5f6
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-30 06:27:19 +0600

    featured-part-start-working-on-service.json.create.

--- src/components/Features/Services.jsx
+++ src/components/Features/Services.jsx
@@ -1,20 +1,36 @@
-import ServicesData from "./Services.json"
-function Services({ Icons }) {
-    // const AllServices = [...ServicesData.Services, Icons];
-    // console.log('AllServices', [...Icons])
+import React from 'react';
+import ServicesData from './Services.json';
+import { FaLeaf, FaLock, FaAnchor, FaCalendarAlt, FaDropbox, FaPaperPlane } from 'react-icons/fa';
+
+const iconComponents = {
+    FaLeaf,
+    FaLock,
+    FaAnchor,
+    FaCalendarAlt,
+    FaDropbox,
+    FaPaperPlane
+};
+
+function Services() {
     return (
         <div>
-            {
-                Services.map(service => {
-                    return (
-                        <>
-
-                        </>
-                    )
-                })
-            }
+            {ServicesData.Services.map((service, index) => {
+                const { icon, name, description } = service;
+                const IconComponent = iconComponents[icon]; // Get the corresponding icon component
+                return (
+                    <div key={index} className='flex flex-col'>
+                        <div>
+                            <IconComponent />
+                        </div>
+                        <div className=''>
+                            <h3 className='text-lg text-Secondary font-bold mb-3 lora'>{name}</h3>
+                            <p className='text-Primary raleway'>{description}</p>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
-    )
+    );
 }
 
-export default Services
+export default Services;
