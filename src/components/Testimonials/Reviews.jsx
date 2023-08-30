a35029c97955a87946f56e97c722503bb77492bd
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-30 18:21:54 +0600

    company-page-done.testimonial-page-.review.json.filte.create-

--- src/components/Testimonials/Reviews.jsx
+++ src/components/Testimonials/Reviews.jsx
@@ -1,23 +1,29 @@
-
+import ALlReviews from "./Reviews.json"
 function Reviews() {
     return (
         <div className='flex items-center justify-evenly ralewaye'>
-            <div>
-                <div className='bg-white shadow rounded p-8'>
-                    <h1>Been trying glimpses journal app. Loved the minimalist design & easy use.</h1>
-                </div>
-                <div className='flex space-x-3 items-center mt-5 ml-10'>
-                    <div className='rounded-full'>
-                        {/* <p className='h-10 w-10 bg-white'></p> */}
-                        <img className='w-10 h-10 rounded-full' src="https://getglimpses.com/assets/img/twitter3.jpg" alt="" />
-                    </div>
-                    <div className=''>
-                        <h2 className='text-xl text-Secondary font-bold mb-[2px]'>Rami Sarieddine</h2>
-                        <p className='text-xs text-Primary'>@RamieSays</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+            {
+                ALlReviews.Reviews.map(review => {
+                    return (
+                        <div key={review.name}>
+                            <div className='bg-white shadow rounded p-8'>
+                                <h1>Been trying glimpses journal app. Loved the minimalist design & easy use.</h1>
+                            </div>
+                            <div className='flex space-x-3 items-center mt-5 ml-10'>
+                                <div className='rounded-full'>
+                                    {/* <p className='h-10 w-10 bg-white'></p> */}
+                                    <img className='w-10 h-10 rounded-full' src="https://getglimpses.com/assets/img/twitter3.jpg" alt="" />
+                                </div>
+                                <div className=''>
+                                    <h2 className='text-xl text-Secondary font-bold mb-[2px]'>Rami Sarieddine</h2>
+                                    <p className='text-xs text-Primary'>@RamieSays</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div >
     )
 }
 
