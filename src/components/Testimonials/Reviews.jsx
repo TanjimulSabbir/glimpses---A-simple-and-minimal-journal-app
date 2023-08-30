3b01cb9ccf196e3fc810cf2f850e054582890b34
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-30 18:19:03 +0600

    company-page-done.testimonial-page-.review.json.filte.create-

--- src/components/Testimonials/Reviews.jsx
+++ src/components/Testimonials/Reviews.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
 
 function Reviews() {
     return (
-        <div>
-            <div className='flex items-center justify-evenly bg-white shadow rounded p-8'>
-                <h1>Been trying glimpses journal app. Loved the minimalist design & easy use.</h1>
-            </div>
-            <div className='flex space-x-2 items-center'>
-                <div className='rounded-full'>
-                    {/* <p className='h-10 w-10 bg-white'></p> */}
-                    <img className='w-10 h-10' src="https://getglimpses.com/assets/img/twitter3.jpg" alt="" />
+        <div className='flex items-center justify-evenly ralewaye'>
+            <div>
+                <div className='bg-white shadow rounded p-8'>
+                    <h1>Been trying glimpses journal app. Loved the minimalist design & easy use.</h1>
                 </div>
-                <div>
-                    <h2>Rami Sarieddine</h2>
-                    <p>@RamieSays</p>
+                <div className='flex space-x-3 items-center mt-5 ml-10'>
+                    <div className='rounded-full'>
+                        {/* <p className='h-10 w-10 bg-white'></p> */}
+                        <img className='w-10 h-10 rounded-full' src="https://getglimpses.com/assets/img/twitter3.jpg" alt="" />
+                    </div>
+                    <div className=''>
+                        <h2 className='text-xl text-Secondary font-bold mb-[2px]'>Rami Sarieddine</h2>
+                        <p className='text-xs text-Primary'>@RamieSays</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -3,7 +3,7 @@ import Reviews from "./Reviews"
 const Testimonials = () => {
     return (
         <div className="my-24 bg-GrayBg">
-            <div className="text-center">
+            <div className="text-center mb-10">
                 <h1 className="text-[32px] font-bold lora py-10">Testimonials</h1>
                 <p className="text-Primary">
                     You are in a great company. <br />
