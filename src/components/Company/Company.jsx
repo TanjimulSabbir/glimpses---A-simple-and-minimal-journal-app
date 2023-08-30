5ed340148cba40daf2f693d9c169b3bac813429d
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-30 06:58:38 +0600

    featured-part-done.company-page-start

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import microsoft from "../../assets/Photos/Company/ms-logo-gray.svg";
+import zapier from "../../assets/Photos/Company/zapier.png";
+import matelogo from "../../assets/Photos/Company/mtelogo.svg";
+import thetimesnews from "../../assets/Photos/Company/thetimesnews.png";
 function Company() {
-  return (
-    <div>
-        
-    </div>
-  )
+    const Companies = [microsoft, zapier, matelogo, thetimesnews]
+    return (
+        <div className="my-14">
+            <div>
+                {
+                    Companies.map((company, index) => {
+                        return (
+                            <div key={index}>
+                                <img src={company} alt={company} />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
 }
 
 export default Company
