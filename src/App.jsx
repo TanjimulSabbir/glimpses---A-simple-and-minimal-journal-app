da1d1a1c70dc5da2287cdc9b09113f733823f4f4
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-30 19:57:17 +0600

    contact-page-on-working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Company from './components/Company/Company'
+import Contact from './components/Contact/Contact'
 import Features from './components/Features/Features'
 import Navbar from './components/Header/Navbar'
 import Testimonials from './components/Testimonials/Testimonials'
@@ -13,6 +14,7 @@ function App() {
       <Features/>
       <Company/>
       <Testimonials/>
+      <Contact/>
     </>
   )
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,17 +1,17 @@
 
 function Contact() {
     return (
-        <div className="py-10 raleway">
-            <div className="text-center mb-4">
-                <h1 className="text-2xl font-extrabold lora mb-10">Join our mailing list to stay up to date</h1>
-                <p className="text-Primary">
-                    Be the first to hear about the latest product releases & other exclusive content. Don't worry, we'll never flood your inbox
+        <div className="py-14">
+            <div className="text-center mb-10">
+                <h1 className="text-2xl font-extrabold lora mb-8">Join our mailing list to stay up to date</h1>
+                <p className="text-Primary text-center">
+                    Be the first to hear about the latest product releases & other exclusive  <br /> content. Don't worry, we'll never flood your inbox
                 </p>
             </div>
-            <div className="w-full max-w-6xl mx-auto">
-                <form action="">
-                    <input type="text" placeholder="Your Email" />
-                    <button>Subscribe</button>
+            <div className="w-full max-w-6xl mx-auto flex justify-center">
+                <form>
+                    <input className="py-3 bg-BtnGray rounded px-3 w-64 placeholder:pl-1 placeholder:text-Primary"  type="email" placeholder="Your Email" />
+                    <button className="text-white text-sm uppercase ml-3 rounded py-3 px-6 font-bold  bg-Blue">Subscribe</button>
                 </form>
             </div>
         </div>
